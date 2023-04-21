9d37f97df5bcccb6fda183c90efd7c6553757e2e
Lucas Dourado <lucas.avila.bsb@gmail.com>
2023-04-21 18:30:32 -0400

    Added new images

--- html/js/funcs.js
+++ html/js/funcs.js
@@ -6,9 +6,8 @@ function populate(dirName, width, margin, toDisplay, displayDiv, cla){
         success: function (data){
             let files = JSON.parse(data);
             for (const i in files) {
-                console.log(files[i]);
                 if (toDisplay){
-                    $('#' + dirName + 'div').append('<img ' + cla + ' style= "margin: ' + margin + '; text-align: center; width: ' + width + '" src="/images/' + dirName + '/' + files[i] + '" onclick="display(\'' + displayDiv +  '\', \'/images/' + dirName + '/' + files[i] + '\')">');
+                    $('#' + dirName + 'div').append('<img ' + cla + ' style= "margin: ' + margin + '; text-align: center; width: ' + width + '" src="/images/lowres/' + files[i] + '" onclick="display(\'' + displayDiv +  '\', \'/images/' + dirName + '/' + files[i] + '\')">');
                 } else {
                     $('#' + dirName + 'div').append('<img ' + cla + ' style= "margin: ' + margin + '; text-align: center; width: ' + width + '" src="/images/' + dirName + '/' + files[i] + '">');
                 }
